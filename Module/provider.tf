4fa3a3d86ddc9eb8122c2eee3c21bf9c3d2573c5
Vivek6790 <134435670+Vivek6790@users.noreply.github.com>
2025-08-16 23:27:54 +0530

    Update provider.tf

--- Module/provider.tf
+++ Module/provider.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
+  
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.38.1"
     }
   }
+backend azurerm {
+resource_group_name = ""
+storage_account = ""
+container = ""
+key = 
 }
 
 provider "azurerm" {
